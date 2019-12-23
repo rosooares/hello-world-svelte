f0ad3358afc76d7b24e99ffca3b4255883b3c35a
Kestraa <kestraa@MacBook-Pro-de-FVFYV6XLL40Y.local>
2019-12-23 10:50:00 -0300

    inital commit

---
+++ security/src/Login.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let username = "";
+	export let password = "";
+	function submit(e) {
+		e.preventDefault();
+		const obj = {
+			'username': username,
+			'password': password,
+		};
+		console.log('obj', obj);
+	}
+</script>
+
+<form>
+	<input type="text" name={username} bind:value={username} placeholder="Username"/>
+	<input type="password" name={password} bind:value={password} placeholder="Password"/>
+	<button type="text" value="Login" on:click={submit}> Login </button>
+</form>
+
+<style>
+	form {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+</style>
